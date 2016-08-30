d126eccfb070096b07403553b29e7c4e139f47fd
Emanuele Busi <emanuele.busi@mondora.com>
2016-08-30 18:11:40 +0200

    Added event log

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,10 +1,12 @@
 import {saveSurvey} from "steps/save-survey";
 import {saveQuestionnaire} from "steps/save-questionnaire";
 import {skip} from "steps/skip";
+import log from "services/logger";
 
 export default async function pipeline (event) {
 
     const element = event.data.element;
+    log.info(element, "event");
 
     // skip if meter or user are not on PostgresDB
     if (!element.type || await skip(element)) {
